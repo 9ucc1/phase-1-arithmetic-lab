6c931f94e9989c6d0a06085366619f442166db7e
Gucci Fan <gucc@umich.edu>
2022-06-29 16:02:03 -0400

    commit tree fb7c89a479f86a339040ba82e69779e3b97295e8

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+let num1 = Math.floor(Math.random() * 10) + 1
+let num2 = 62/num1
+let multiply = num1 * num2
 
-// Write your code here
+let random = Math.floor(Math.random()*10)+1
+
+let num3 = Math.floor(Math.random()*10)+1
+let num4 = num3 * 0.25
+let mod = num3 / num4
+
+let max = Math.max(1, 2, 3, 4, 20)
